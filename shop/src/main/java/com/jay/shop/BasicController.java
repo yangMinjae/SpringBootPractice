65311197b0c2f5f5617964321e202534217b65d1
yangMinjae <ymj137955@naver.com>
2025-12-23 15:29:24 +0900

    .

--- shop/src/main/java/com/jay/shop/BasicController.java
+++ shop/src/main/java/com/jay/shop/BasicController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 @Controller
 public class BasicController {
     @GetMapping("/")
-    @ResponseBody
+    // @ResponseBody        // return에 있는 문자를 그대로 보내라
     String function(){
-        return "안녕하세요";
+        return "index.html";    // static 폴더가 기본 경로
     }
 
     @GetMapping("/2")
@@ -26,5 +30,13 @@ public class BasicController {
     @ResponseBody
     String function4(){
         return "<h4>하하하하</h4>";
+    }
+
+    @GetMapping("/date")
+    @ResponseBody
+    String function5(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return now.format(formatter);
     }
 }
