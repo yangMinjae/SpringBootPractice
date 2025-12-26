7f2d9b2a08aa5b71e1cac0077533164093e0fd69
yangMinjae <ymj137955@naver.com>
2025-12-26 23:39:35 +0900

    12월 26일 spring boot 실습

--- shop/src/main/java/com/jay/shop/Item.java
+++ shop/src/main/java/com/jay/shop/Item.java
@@ -1,4 +1,19 @@
 package com.jay.shop;
 
+import jakarta.persistence.*;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@ToString
 public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    @Column(nullable = false, unique = true, columnDefinition = "TEXT")
+    //@Column(length = 1000)
+    public String title;
+    public Integer price;
+    public LocalDateTime date;
 }
+
--- shop/src/main/java/com/jay/shop/ItemController.java
+++ shop/src/main/java/com/jay/shop/ItemController.java
@@ -1,14 +1,30 @@
 package com.jay.shop;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+//    @Autowired
+//    public ItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+
     @GetMapping("/list")
     String list(Model model){
-        model.addAttribute("name","홍길동");
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items",result);
+        var a = new Item();
+        System.out.println(a);
         return "list.html";
     }
 }
