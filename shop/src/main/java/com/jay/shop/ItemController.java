ff927fd54e93710ef594be1c9e0a19cedf815fd0
yangMinjae <ymj137955@naver.com>
2025-12-27 23:12:03 +0900

    2025.12.27 boot 실습

--- shop/src/main/java/com/jay/shop/ItemController.java
+++ shop/src/main/java/com/jay/shop/ItemController.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,4 +30,38 @@ public class ItemController {
         System.out.println(a);
         return "list.html";
     }
+    @GetMapping("/write")
+    String write(){
+        return "write.html";
+    }
+
+//    @PostMapping("/add")
+//    String addPost(@RequestParam(name="title") String title, String price){
+//        System.out.println(title);
+//        System.out.println(price);
+//        return "redirect:/list";
+//    }
+    @PostMapping("/add")
+    String addPost(@RequestParam Map formData){
+        System.out.println(formData);
+        String name = formData.get("title").toString();
+        Integer price = Integer.parseInt(formData.get("price").toString());
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("상품명은 필수입니다.");
+        }
+
+        if (name.length() < 2 || name.length() > 50) {
+            throw new IllegalArgumentException("상품명 길이 오류");
+        }
+
+        if (price == null || price <= 0) {
+            throw new IllegalArgumentException("가격은 0보다 커야 합니다.");
+        }
+        Item it = new Item();
+        it.title = name;
+        it.price = price;
+        itemRepository.save(it);
+        return "redirect:/list";
+    }
+
 }
