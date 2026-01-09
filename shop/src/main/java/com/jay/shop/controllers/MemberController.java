49625a2e72e39cdaf9b1175a77cbddc77a57a5e1
yangMinjae <ymj137955@naver.com>
2026-01-09 22:53:28 +0900

    2026.01.09 boot 실습

--- shop/src/main/java/com/jay/shop/controllers/MemberController.java
+++ shop/src/main/java/com/jay/shop/controllers/MemberController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,7 +14,10 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/member")
-    public ResponseEntity<String> register(MemberJoinDTO mDTO){
+    public ResponseEntity<String> register(@RequestBody MemberJoinDTO mDTO){
+        System.out.println(mDTO.getName());
+        System.out.println(mDTO.getLoginId());
+        System.out.println(mDTO.getPassword());
         memberService.saveMember(mDTO);
         return ResponseEntity.ok("회원가입 성공");
     }
